5d4b4f4e195e8392f8c12e95901c7ccd813d441e
Marc Levine <marc.levine@market6.com>
2017-07-05 11:55:45 -0500

    initial commit

---
+++ src/app/alerts/warningAlert/warningAlert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+    <div class="alert alert-warning">
+      <strong>Thou Shalt Not Pass</strong>
+    </div>`
+})
+export class WarningAlertComponent {
+
+}
